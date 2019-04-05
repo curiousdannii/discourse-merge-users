480cad9dbf9fe927607fd882e5bc4856776aec00
Dannii Willis <curiousdannii@gmail.com>
2019-04-06 09:14:06 +1000

    Begin working on the merge-users plugin

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-merge-users
+# about: Adds an admin option to merge users
+# version: 0.1.0
+# authors: Dannii Willis
+# url: https://github.com/curiousdannii/discourse-merge-users
