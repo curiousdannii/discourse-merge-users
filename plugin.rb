38eda669fadb037c6e520e86c7f3d6d93c2dfaa5
Dannii Willis <curiousdannii@gmail.com>
2019-04-06 13:26:13 +1000

    Make a custom page instead

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,9 @@
 # version: 0.1.0
 # authors: Dannii Willis
 # url: https://github.com/curiousdannii/discourse-merge-users
+
+add_admin_route 'merge-users.title', 'merge-users'
+
+Discourse::Application.routes.append do
+    get '/admin/plugins/merge-users' => 'admin/plugins#index', constraints: StaffConstraint.new
+end
